85fc2735a5340b1d31113db8585eaa56b0714c8f
kewong <kenz.yihhang@gmail.com>
2021-06-09 15:14:05 +0800

    added default and yml profile

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishGreetingService.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile({"ES", "default"})
+@Profile("ES")
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
 
